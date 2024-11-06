ced698fe187448fd7b8a86ca35ec06bf13299650
poweq <poweq68@gmail.com>
2024-11-06 09:38:51 +0900

    clean agv folder

--- AGV_team/ros2_ws/build/ps2_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ysh/KG_2_Project/AGV_team/ros2_ws/src/ps2_controller
-# Build directory: /home/ysh/KG_2_Project/AGV_team/ros2_ws/build/ps2_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
