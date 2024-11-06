a4bd46636d30be5fa8fa4be2228ea57c9f834b75
poweq <poweq68@gmail.com>
2024-11-06 09:34:06 +0900

    add ros2 ps2 node

---
+++ AGV_team/ros2_ws/build/ps2_controller/CMakeFiles/ps2_controller_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ps2_controller_node.dir/src/ps2_controller.cpp.o"
+  "CMakeFiles/ps2_controller_node.dir/src/ps2_controller_node.cpp.o"
+  "ps2_controller_node"
+  "ps2_controller_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ps2_controller_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ AGV_team/ros2_ws/build/ps2_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ysh/KG_2_Project/AGV_team/ros2_ws/src/ps2_controller
+# Build directory: /home/ysh/KG_2_Project/AGV_team/ros2_ws/build/ps2_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
